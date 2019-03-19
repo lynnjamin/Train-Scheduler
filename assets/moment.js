93fd762260f2f95a8f0a444bcd7c37f99825fe99
Lynn Tang <tanglynn2@gmail.com>
2019-03-18 20:14:55 -0500

    html and css done

---
+++ assets/moment.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDNBGy0QVKe3j7nLC6MrMLc5ihYLABWLn4",
+    authDomain: "train-scheduler-8da34.firebaseapp.com",
+    databaseURL: "https://train-scheduler-8da34.firebaseio.com",
+    projectId: "train-scheduler-8da34",
+    storageBucket: "train-scheduler-8da34.appspot.com",
+    messagingSenderId: "515399862927"
+  };
+  firebase.initializeApp(config);
+
+
